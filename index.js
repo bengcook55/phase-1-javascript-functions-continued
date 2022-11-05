7e622d999e327c9f223f37a7895ac736eee531d7
Benjamin Cook <bengcook55@gmail.com>
2022-11-05 17:00:53 -0400

    commit tree 22f9c7d882db217086a8678edc50602109e32d6f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+// code your solution here\
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+
+function mondayWork(work = "go to the office"){
+    return `This Monday, I will ${work}.`
+}
+
+
+function wrapAdjective(style = "*"){
+    return function(adj= "special") {
+        return `You are ${style}${adj}${style}!`
+    }
+}
